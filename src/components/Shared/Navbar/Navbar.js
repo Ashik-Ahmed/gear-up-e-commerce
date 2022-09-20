fb77ba047a200a74cce5855223608135cfa52220
ASHIK AHMED <ashikahmed121@gmail.com>
2022-09-20 15:09:03 +0600

    navbar updated

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -29,16 +29,6 @@ const Navbar = () => {
                             <li><Link to='/reviews'>Reviews</Link></li>
                             <li><Link to='/blogs'>Blogs</Link></li>
                             <li><Link to='/contact-us'>Contact Us</Link></li>
-                            {/* <li tabindex="0">
-                                <a class="justify-between">
-                                    Parent
-                                    <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                                </a>
-                                <ul class="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li> */}
                         </ul>
                     </div>
                     <Link to='/' class="normal-case text-xl text-cyan-600 font-extrabold ml-4 italic">Gear Up</Link>
@@ -49,29 +39,9 @@ const Navbar = () => {
                         <li><Link to='/reviews'>Reviews</Link></li>
                         <li><Link to='/blogs'>Blogs</Link></li>
                         <li><Link to='/contact-us'>Contact Us</Link></li>
-                        {/* <li tabindex="0">
-                            <a>
-                                Parent
-                                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                            </a>
-                            <ul class="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li> */}
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    {/* {
-                        authUser ?
-
-                            <div>
-                                <button onClick={handleSignout} className='btn btn-sm bg-indigo-500 hover:bg-indigo-600 border-0'>Signout</button>
-                                <p className='text-xs'>{authUser.email}</p>
-                            </div>
-                            :
-                            <Link to='/login' className="btn btn-sm bg-gray-600 border-0">Login</Link>
-                    } */}
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
